4fded34d68b81be357ac3fd66971f2c4d99c45a6
Volkan Özçelik <me@volkan.io>
2023-01-19 21:37:00 -0800

    added log level

--- env/env.go
+++ env/env.go
@@ -173,3 +173,28 @@ func SafeAgeKeyPath() string {
 	}
 	return p
 }
+
+// SafeLogLevel returns the value set by AEGIS_SAFE_LOG_LEVEL environment
+// variable, or a default level.
+//
+// AEGIS_SAFE_LOG_LEVEL determines the verbosity of the logs.
+// 1: logs are off, 6: highest verbosity.
+// Off = 1, Error = 2, Warn = 3, Info = 4, Debug = 5, Trace = 6
+func SafeLogLevel() int {
+	p := os.Getenv("AEGIS_SAFE_LOG_LEVEL")
+	if p == "" {
+		return 3
+	}
+	l, _ := strconv.Atoi(p)
+	if l == 0 {
+		return 3
+	}
+	if l < 0 || l > 6 {
+		return 3
+	}
+	return l
+}
+
+
+- name: AEGIS_SAFE_LOG_LEVEL
+value: "6"
