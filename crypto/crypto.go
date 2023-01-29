52e5c22cb450ae70f5e8d9d72098f4dcdfae130b
Volkan Özçelik <me@volkan.io>
2023-01-29 07:26:12 -0800

    correlation id

---
+++ crypto/crypto.go
@@ -0,0 +1,46 @@
+/*
+ * .-'_.---._'-.
+ * ||####|(__)||   Protect your secrets, protect your business.
+ *   \\()|##//       Secure your sensitive data with Aegis.
+ *    \\ |#//                  <aegis.z2h.dev>
+ *     .\_/.
+ */
+
+package crypto
+
+import (
+	crand "crypto/rand"
+	"math/rand"
+	"time"
+)
+
+// RandomString generates a unique-enough random string to be used for correlating
+// entities. Note that RandomString is NOT cryptographically secure and should
+// NOT be used in sensitive operations. For sensitive operations, consider
+// RandomStringSecure, which is cryptographically secure, but slower.
+func RandomString(n int) (string, error) {
+	rand.Seed(time.Now().UnixNano())
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes), nil
+}
+
+// RandomStringSecure creates a unique and cryptographically-secure random
+// string. For a faster (and insecure) alternative, consider RandomString.
+func RandomStringSecure(n int) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	bytes := make([]byte, n)
+	if _, err := crand.Read(bytes); err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes), nil
+}
