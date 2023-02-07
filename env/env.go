d5d2b6809dd0b5e82f6a56430e859e3e18a1f1c0
Volkan Özçelik <me@volkan.io>
2023-02-06 22:45:00 -0800

    breaking: renamed AEGIS_SAFE_TLS_PORT as AEGIS_TLS_PORT

--- env/env.go
+++ env/env.go
@@ -141,11 +141,11 @@ func SafeSvidRetrievalTimeout() time.Duration {
 	return time.Duration(i) * time.Millisecond
 }
 
-// SafeTlsPort returns the secure port for Aegis Safe to listen on.
-// It checks the AEGIS_SAFE_TLS_PORT environment variable. If the variable
+// TlsPort returns the secure port for Aegis Safe to listen on.
+// It checks the AEGIS_TLS_PORT environment variable. If the variable
 // is not set, it defaults to ":8443".
-func SafeTlsPort() string {
-	p := os.Getenv("AEGIS_SAFE_TLS_PORT")
+func TlsPort() string {
+	p := os.Getenv("AEGIS_TLS_PORT")
 	if p == "" {
 		p = ":8443"
 	}
